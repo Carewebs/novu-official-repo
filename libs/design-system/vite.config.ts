75649da94bd0e7c393bc0cba118354a6ba51fcbd
Joel Anton <joel@novu.co>
2024-02-05 12:18:45 -0800

    fix: Include CSS in output

--- libs/design-system/vite.config.ts
+++ libs/design-system/vite.config.ts
@@ -4,10 +4,17 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import dts from 'vite-plugin-dts';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ include: ['src'] })],
+  plugins: [
+    react(),
+    // ensure CSS is included in build output
+    libInjectCss(),
+    // generate .d.ts files
+    dts({ include: ['src'] }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
